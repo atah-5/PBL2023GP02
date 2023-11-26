b78e4142cc599815450846893e43700e743da7ba
21026181-m <21026181@mmm.muroran-it.ac.jp>
2023-11-26 12:43:34 +0900

    微妙に変更

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,21 @@
-
+from flask import render_template
 from PBLapp import app
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+events =[
+    {
+        'title':'event1',
+        'date':'2023-12-11'
+    },
+    {
+        'title':'event2',
+        'date':'2023-12-01'
+    }
+]
+
+
+#@app.route('/main')
+def calendar():
+    return render_template('main.html',events=events)
